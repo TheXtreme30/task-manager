856af81f6e58611ce04be58dea70fa189bc32773
Sergey <thextreme.30@gmail.com>
2022-07-06 19:41:44 +0600

    fixed templates

--- user/views.py
+++ user/views.py
@@ -8,29 +8,29 @@ from django.contrib.auth.decorators import login_required
 
 def signupuser(request):
     if request.method == 'GET':
-        return render(request, 'Task/signupuser.html', {'form':UserCreationForm()})
+        return render(request, 'user/signupuser.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
-                return redirect('currentTasks')
+                return redirect('currenttasks')
             except IntegrityError:
-                return render(request, 'Task/signupuser.html', {'form':UserCreationForm(), 'error':'That username has already been taken. Please choose a new username'})
+                return render(request, 'user/signupuser.html', {'form':UserCreationForm(), 'error':'That username has already been taken. Please choose a new username'})
         else:
-            return render(request, 'Task/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+            return render(request, 'user/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
 
 def loginuser(request):
     if request.method == 'GET':
-        return render(request, 'Task/loginuser.html', {'form':AuthenticationForm()})
+        return render(request, 'user/loginuser.html', {'form':AuthenticationForm()})
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'Task/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+            return render(request, 'user/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
         else:
             login(request, user)
-            return redirect('currentTasks')
+            return redirect('currenttasks')
 
 @login_required
 def logoutuser(request):
